26571f66be4c267739895f3e30e3c9fd820d284e
yongjialiang <yongjl-wa15@student.tarc.edu.my>
2018-12-26 18:19:09 +0800

    initial

---
+++ app/src/main/java/com/example/taruc/lab42database/UserViewModel.java
@@ -0,0 +1,33 @@
+package com.example.taruc.lab42database;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+//TODO 7: Create an Android View Model class to link data to UI
+
+//get data whether from local or server
+public class UserViewModel extends AndroidViewModel {
+    private UserRepository userRepository;
+    private LiveData<List<User>> allUsers; // A cache copy of record
+
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+        userRepository = new UserRepository(application);
+        allUsers = userRepository.getAllUsers();
+    }
+
+    public LiveData<List<User>> getAllUsers() {
+        return allUsers;
+    }
+
+    public  void insertUser(User user){
+        userRepository.insertUser(user);
+    }
+
+
+    public void deleteWord(User user) {userRepository.deleteUser(user);}
+}
